92c58f11b1a64cdfc279015a3fc6f440459a0508
Håkon Bjørgen Refsvik <hakonbjorgenrefsvik@Hakons-Mac-mini.local>
2024-07-25 10:24:47 +0200

    removed imports

--- src/app/(tabs)/(home)/BottomSheet.tsx
+++ src/app/(tabs)/(home)/BottomSheet.tsx
@@ -1,15 +1,9 @@
 import {Dimensions, StyleSheet, Text, View} from "react-native"
 import React, { useCallback, useEffect, useImperativeHandle } from "react"
-import { defaultStyles } from "@/styles"
 import Spacer from "./Spacer";
 import Animated, { useAnimatedStyle, useSharedValue, withSpring, withTiming } from "react-native-reanimated";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import { colors, fontSize } from "@/constants/tokens";
-import { InputField } from "./InputField";
-import { TankCard } from "./TankCard";
-import { TankIcon12L, TankIcon18L, TankIcon24L, TankIcon26L, TankIcon27L, TankIcon33L} from './TankSVG';
-
-import TankList from "./TankList";
 
 const {height: SCREEN_HEIGHT} = Dimensions.get("window");
 export const MAX_Y = -SCREEN_HEIGHT
